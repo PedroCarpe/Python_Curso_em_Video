ca6eeb7364ba763886c23bf146b315d2f96b1041
Pedro Carpe <dd2426016@gmail.com>
2022-11-20 15:30:43 -0300

    Algoritmos Python: Curso em Video

---
+++ ex6.py
@@ -0,0 +1,10 @@
+import math
+
+a = int(input('Digite um número: '))
+print(f'\nO dobro de {a} vale {2*a}')
+print(f'O triplo de {a} vale {3*a}')
+print(f'A raiz quadrada de {a} vale {math.sqrt(a):.2f}')
+
+
+#Aplicação do conceito do exercício, notas musicais e frequência
+#print('Dado, que a frequência da nota musical dó, é de 132 Hz. E sua oitava superior, possui o dobro dessa frequência, qual é o valor da frequência da oitava superior?')
