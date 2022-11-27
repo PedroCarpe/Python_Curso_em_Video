6bdb35a56d37a2877a48f8439529b757a273cb13
Pedro Carpe <dd2426016@gmail.com>
2022-11-26 23:47:44 -0300

    Algoritmos Python: Curso em Video

---
+++ ex8.py
@@ -0,0 +1,19 @@
+import math
+
+print('___Conversão de medidas de comprimento___\nUnidades disponíveis:km hm dam m dm cm mm')
+unidade1,unidade2 = input('\nDigite as unidades a serem convertidas, respectivamente: ').split()
+valor = float(input('Digite o valor a ser convertido: '))
+
+unidades=['km','hm','dam','m','dm','cm','mm']
+for x in range(len(unidades)):
+    if (unidade1)==unidades[x]:
+        a=x
+    if (unidade2)==unidades[x]:
+        b=x
+
+c=math.fabs(a-b)
+if (a-b)<0:
+    print(f'\nResposta: {valor:.2f}{unidade1} equivale a {valor*math.pow(10,c):.2f}{unidade2}')
+
+if (a-b)>0:
+    print(f'\nResposta: {valor:.2f}{unidade1} equivale a {valor*(1/math.pow(10,c)):.2f}{unidade2}')
