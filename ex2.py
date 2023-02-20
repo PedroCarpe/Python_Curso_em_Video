6e1b64532b63ee1433fe5c321ea0a6773e061097
PedroCarpe <97778087+PedroCarpe@users.noreply.github.com>
2023-02-20 18:47:45 -0300

    Delete ex2.py

--- ex2.py
+++
@@ -1,8 +0,0 @@
-#Curso em Vídeo: Curso de Python, ex02
-
-nome=input('Digite seu nome: ')
-print(f'É um prazer te conhecer, {nome}!')
-
-#outra possibilidade
-nome=input('Digite seu nome: ')
-print('É um prazer te conhecer, {}!'.format(nome))
